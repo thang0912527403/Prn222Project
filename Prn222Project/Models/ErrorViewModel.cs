ec5ab5cb056ff59ea63b4c556a204913ef2d9ca9
Dương Xuân Thắng <thang0912527403@gmail.com>
2025-05-15 11:13:02 +0700

    Add project files.

---
+++ Prn222Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prn222Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
