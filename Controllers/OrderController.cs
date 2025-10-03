caa96aafe627ce1f167f15c354c40eda0d454e5f
mquang2004 <quanglmhe180502@fpt.edu.vn>
2025-10-04 01:04:13 +0700

    update frontend

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Prn222Project.Data;
+using System.Security.Claims;
+namespace Prn222Project.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderController(ApplicationDbContext db) => _db = db;
+
+        // /Orders
+        public async Task<IActionResult> Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+            var orders = await _db.Orders
+                .Where(o => o.UserId == userId)
+                .Include(o => o.OrderDetails)
+                .OrderByDescending(o => o.order_date)
+                .ToListAsync();
+
+            return View(orders);
+        }
+
+        // /Orders/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+            var order = await _db.Orders
+                .Where(o => o.UserId == userId && o.order_id == id)
+                .Include(o => o.OrderDetails)
+                    .ThenInclude(od => od.ProductVariant)
+                        .ThenInclude(pv => pv.Product)
+                .FirstOrDefaultAsync();
+
+            if (order == null) return NotFound(); 
+
+            return View(order);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ using Prn222Project.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var config = builder.Configuration;
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -62,7 +61,6 @@ builder.Services.AddTransient<IEmailSender, EmailSender>();
 builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -70,13 +68,12 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-// loi 403 or 404
+
 app.UseStatusCodePagesWithRedirects("/");
 app.UseRouting();
 
